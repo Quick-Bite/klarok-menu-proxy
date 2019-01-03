5f5e9cfc45c401956244cd31ef7207a4fb33055a
Ammar <ammar.alnatsheh@gmail.com>
2019-01-03 14:20:25 -0800

    implement reviews in the proxy server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const httpReq = require('axios');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
@@ -6,6 +7,22 @@ const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/restaurants/:id', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/restaurants/:id/reviews', (req, res) => {
+  // get all the reviews from the review server
+  httpReq.get(`http://localhost:3004/restaurants/${req.params.id}/reviews`)
+  .then(function (req) {
+    res.status(200).send(req.data);
+  })
+  .catch(function (error) {
+    res.status(501).send(error);
+  });
+
+});
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
