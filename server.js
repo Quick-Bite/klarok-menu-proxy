c28f6d782d14573e7af53a84df924f40635794bd
Ammar <ammar.alnatsheh@gmail.com>
2019-01-03 17:16:54 -0800

    add suggestions and menu components

--- server.js
+++ server.js
@@ -13,8 +13,56 @@ app.get('/restaurants/:id', (req, res) => {
 });
 
 app.get('/restaurants/:id/reviews', (req, res) => {
-  // get all the reviews from the review server
+  // get all the reviews from the reviews server
   httpReq.get(`http://localhost:3004/restaurants/${req.params.id}/reviews`)
+  .then(function (req) {
+    res.status(200).send(req.data);
+  })
+  .catch(function (error) {
+    res.status(501).send(error);
+  });
+
+});
+
+app.get('/restaurants/:id/Suggestions', (req, res) => {
+  // get all the Suggestions from the suggestions server
+  httpReq.get(`http://localhost:3003/restaurants/${req.params.id}/Suggestions`)
+  .then(function (req) {
+    res.status(200).send(req.data);
+  })
+  .catch(function (error) {
+    res.status(501).send(error);
+  });
+
+});
+
+app.get('/restaurants/:id/menu-items', (req, res) => {
+  // get all the menu items from the menu server
+  httpReq.get(`http://localhost:3002/restaurants/${req.params.id}/menu-items`)
+  .then(function (req) {
+    res.status(200).send(req.data);
+  })
+  .catch(function (error) {
+    res.status(501).send(error);
+  });
+
+});
+
+app.get('/restaurants/:id/menu-items/:itemId', (req, res) => {
+  // get all the menu item Id from the menu server
+  httpReq.get(`http://localhost:3002/restaurants/${req.params.id}/itemId`)
+  .then(function (req) {
+    res.status(200).send(req.data);
+  })
+  .catch(function (error) {
+    res.status(501).send(error);
+  });
+
+});
+
+app.get('/restaurants/:id/order', (req, res) => {
+  // get all the menu order from the menu server
+  httpReq.get(`http://localhost:3002/restaurants/${req.params.id}/order`)
   .then(function (req) {
     res.status(200).send(req.data);
   })
