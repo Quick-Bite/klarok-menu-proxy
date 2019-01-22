d3b644b58fc3c4004dfddaa7b19b9512a755ff87
klarok <karaylu@gmail.com>
2019-01-21 16:52:04 -0800

    Adds loadtesting scripts and new relic, changes refs to EC2

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
+require('newrelic');
 const express = require('express');
 const httpReq = require('axios');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(morgan('dev'));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/restaurants/:id', (req, res) => {
   res.status(200).sendFile(path.join(__dirname, './public/index.html'));
@@ -38,7 +39,8 @@ app.get('/restaurants/:id/Suggestions', (req, res) => {
 
 app.get('/restaurants/:id/menu-items', (req, res) => {
   // get all the menu items from the menu server
-  httpReq.get(`http://ec2-13-57-210-63.us-west-1.compute.amazonaws.com/restaurants/${req.params.id}/menu-items`)
+  // httpReq.get(`http://ec2-13-57-210-63.us-west-1.compute.amazonaws.com/restaurants/${req.params.id}/menu-items`)
+  httpReq.get(`http://ec2-13-52-81-151.us-west-1.compute.amazonaws.com:3002/restaurants/${req.params.id}/menu-items`)
   .then(function (req) {
     res.status(200).send(req.data);
   })
@@ -50,7 +52,8 @@ app.get('/restaurants/:id/menu-items', (req, res) => {
 
 app.get('/restaurants/:id/menu-items/:itemId', (req, res) => {
   // get all the menu item Id from the menu server
-  httpReq.get(`http://ec2-13-57-210-63.us-west-1.compute.amazonaws.com/restaurants/${req.params.id}/menu-items/${req.params.itemId}`)
+  // httpReq.get(`http://ec2-13-57-210-63.us-west-1.compute.amazonaws.com/restaurants/${req.params.id}/menu-items/${req.params.itemId}`)
+  httpReq.get(`http://ec2-13-52-81-151.us-west-1.compute.amazonaws.com:3002/restaurants/${req.params.id}/menu-items/${req.params.itemId}`)
   .then(function (req) {
     res.status(200).send(req.data);
   })
@@ -62,7 +65,7 @@ app.get('/restaurants/:id/menu-items/:itemId', (req, res) => {
 
 app.get('/restaurants/:id/order', (req, res) => {
   // get all the menu order from the menu server
-  httpReq.get(`http://ec2-13-57-210-63.us-west-1.compute.amazonaws.com/restaurants/${req.params.id}/order`)
+  httpReq.get(`http://ec2-13-52-81-151.us-west-1.compute.amazonaws.com:3002/restaurants/${req.params.id}/order`)
   .then(function (req) {
     res.status(200).send(req.data);
   })
