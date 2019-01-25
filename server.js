8d52428e2c294d77f1b4cd8d82e579e2aadbee6f
klarok <karaylu@gmail.com>
2019-01-24 16:53:58 -0800

    Runs proxy with all services on EC2

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const TO_REVIEWS = 'ec2-54-153-119-40.us-west-1.compute.amazonaws.com';
+const TO_SUGGESTIONS = '18.188.10.73';
+const TO_MENU = '54.241.249.88';
+const TO_PROFILE = '52.15.232.64';
+
 // app.use(morgan('dev'));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/restaurants/:id', (req, res) => {
   res.status(200).sendFile(path.join(__dirname, './public/index.html'));
@@ -15,7 +20,7 @@ app.get('/restaurants/:id', (req, res) => {
 
 app.get('/restaurants/:id/reviews', (req, res) => {
   // get all the reviews from the reviews server
-  httpReq.get(`http://ec2-13-56-233-213.us-west-1.compute.amazonaws.com/restaurants/${req.params.id}/reviews`)
+  httpReq.get(`http://${TO_REVIEWS}/restaurants/${req.params.id}/reviews`)
   .then(function (req) {
     res.status(200).send(req.data);
   })
@@ -25,9 +30,9 @@ app.get('/restaurants/:id/reviews', (req, res) => {
 
 });
 
-app.get('/restaurants/:id/Suggestions', (req, res) => {
+app.get('/restaurants/:id/suggestions', (req, res) => {
   // get all the Suggestions from the suggestions server
-  httpReq.get(`http://ec2-54-183-207-43.us-west-1.compute.amazonaws.com/restaurants/${req.params.id}/Suggestions`)
+  httpReq.get(`http://${TO_SUGGESTIONS}/restaurants/${req.params.id}/suggestions`)
   .then(function (req) {
     res.status(200).send(req.data);
   })
@@ -39,8 +44,7 @@ app.get('/restaurants/:id/Suggestions', (req, res) => {
 
 app.get('/restaurants/:id/menu-items', (req, res) => {
   // get all the menu items from the menu server
-  // httpReq.get(`http://ec2-13-57-210-63.us-west-1.compute.amazonaws.com/restaurants/${req.params.id}/menu-items`)
-  httpReq.get(`http://ec2-13-52-81-151.us-west-1.compute.amazonaws.com:3002/restaurants/${req.params.id}/menu-items`)
+  httpReq.get(`http://${TO_MENU}/restaurants/${req.params.id}/menu-items`)
   .then(function (req) {
     res.status(200).send(req.data);
   })
@@ -52,8 +56,7 @@ app.get('/restaurants/:id/menu-items', (req, res) => {
 
 app.get('/restaurants/:id/menu-items/:itemId', (req, res) => {
   // get all the menu item Id from the menu server
-  // httpReq.get(`http://ec2-13-57-210-63.us-west-1.compute.amazonaws.com/restaurants/${req.params.id}/menu-items/${req.params.itemId}`)
-  httpReq.get(`http://ec2-13-52-81-151.us-west-1.compute.amazonaws.com:3002/restaurants/${req.params.id}/menu-items/${req.params.itemId}`)
+  httpReq.get(`http://${TO_MENU}/restaurants/${req.params.id}/menu-items/${req.params.itemId}`)
   .then(function (req) {
     res.status(200).send(req.data);
   })
@@ -65,7 +68,7 @@ app.get('/restaurants/:id/menu-items/:itemId', (req, res) => {
 
 app.get('/restaurants/:id/order', (req, res) => {
   // get all the menu order from the menu server
-  httpReq.get(`http://ec2-13-52-81-151.us-west-1.compute.amazonaws.com:3002/restaurants/${req.params.id}/order`)
+  httpReq.get(`http://${TO_MENU}/restaurants/${req.params.id}/order`)
   .then(function (req) {
     res.status(200).send(req.data);
   })
@@ -75,9 +78,9 @@ app.get('/restaurants/:id/order', (req, res) => {
 
 });
 
-app.get('/restaurants/:id/profile', (req, res) => {
+app.get('/restaurants/:id/profiles', (req, res) => {
   // get all the profile from profile server
-  httpReq.get(`http://ec2-18-225-9-230.us-east-2.compute.amazonaws.com/restaurants/${req.params.id}/profile`)
+  httpReq.get(`http://${TO_PROFILE}/restaurants/${req.params.id}/profiles`)
   .then(function (req) {
     res.status(200).send(req.data);
   })
